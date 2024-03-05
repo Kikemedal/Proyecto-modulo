21227eeef10388bc8e958f9bc5db8618b549e1ed
kikemedal <kikemedal@gmail.com>
2024-03-05 13:56:56 +0000

    Se ha completado la pagina de incio

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,21 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- CSS personalizado -->
-    <link href="{{ asset('/css/index.css') }}" rel="stylesheet" />
+    
+
     <!-- CSS Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <!-- CSS personalizado -->
+    <link href="{{ asset('/css/espacio.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/css/index.css') }}" rel="stylesheet" />
     <title>Comienza la aventura</title>
 </head>
 <body class="wrapper">
 
-    <canvas>
-
-    </canvas>
+    <canvas></canvas>
 
 
-    <header class="masthead bg-primary text-white text-center py-4">
-        <div class="container d-flex align-items-center flex-column">
+    <header class="masthead headerbackground text-white text-center py-4">
+        <div class="container d-flex align-items-center flex-column textoh2">
             <h2>Comienza la aventura</h2>
         </div>
     </header>
@@ -26,16 +27,26 @@
     <!--Cuerpo con los enlaces -->
     <div class="d-flex justify-content-center align-items-center vh-100">
         <div class="text-center">
-            <a href="/ruta-listado" class="btn btn-primary btn-lg m-3 centered-link">Ver listado de personajes</a>
-            <a href="/ruta-creacion" class="btn btn-primary btn-lg m-3 centered-link">Crear personaje</a>
-            <a href="/ruta-login" class="btn btn-primary btn-lg m-3 centered-link">Iniciar sesión</a>
+            <a href="/ruta-listado" class="btn btn-lg m-3 centered-link transparente zoom-image">
+                <div class="overlay-text">Personajes</div>
+                <img src="{{asset('/img/marte.png')}}">
+            </a> 
+            <a href="/ruta-creacion" class="btn btn-lg m-3 centered-link transparente zoom-image">
+                <div class="overlay-text">Crear personaje</div>
+                <img src="{{asset('/img/tierra.png')}}">
+            </a>
+            <a href="/ruta-login" class="btn btn-lg m-3 centered-link transparente zoom-image">
+                <div class="overlay-text">Iniciar sesión</div>
+                <img src="{{asset('/img/morado.png')}}">
+            </a>
         </div>
     </div>
+
 
     
     <!-- JS Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <!-- JS personalizado -->
-    <script src="{{ asset('/js/index.js') }}"></script>
+    <script src="{{ asset('/js/espacio.js') }}"></script>
 </body>
 </html>
