1a2e686f402c18f19bedf8b9021995ad7beacfdb
kikemedal <kikemedal@gmail.com>
2024-03-06 13:51:49 +0000

    Autenticación de usuario a medias

---
+++ resources/views/formularioSesion/formularioSesion.blade.php
@@ -0,0 +1,10 @@
+@extends('templates.forms')
+
+@section('subtitle', 'Inicia Sesión')
+
+@section('content')
+
+<!--Contenido de formulario -->
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+}) -> name('index');
 
 
 Route::get('/formularioPersonaje', 'App\Http\Controllers\FormulariosController@index' ) -> name('formularioPersonaje');
